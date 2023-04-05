4ae628881a6d9dc7f34312f2bb6a28beb44bc3c4
AnamikaSanjay <asanjay@intelops.dev>
2023-04-05 03:45:56 -0400

    renamed the main function in kernel program as xdp_prog_func

--- network/Learning XDP/basic02-map-counter/xdp-map-counter/xdp-map-counter.c
+++ network/Learning XDP/basic02-map-counter/xdp-map-counter/xdp-map-counter.c
@@ -5,7 +5,7 @@
 #include "common_kern_user.h" // definiton of datarec and XDP action
 
 char __license[] SEC("license") = "Dual MIT/GPL";
-
+/*
 struct bpf_map_def SEC("maps") xdp_stats_map =
 {
     .type = BPF_MAP_TYPE_ARRAY,
@@ -13,18 +13,27 @@ struct bpf_map_def SEC("maps") xdp_stats_map =
     .value_size =sizeof(struct datarec),
     .max_entries = XDP_ACTION_MAX,
 };
-//definiton of map 
+//definiton of map
+*/
+
+struct {
+    __uint(type, BPF_MAP_TYPE_ARRAY);
+	__uint(max_entries, XDP_ACTION_MAX);
+	__type(key, __u32);  
+	__type(value, sizeof(struct datarec)); 
+} xdp_stats_map SEC(".maps");
+
 
 #ifndef lock_xadd
 #define lock_xadd(ptr,val) ((void) __sync_fetch_and_add(ptr,val))
 #endif
 //using GNU built in __sync_fetch_and_add for sync and adding to bpf map values
 
-SEC("xdp_stats1")
-int xdp_stats1_func(struct xdp_md *ctx)
+SEC("xdp")
+int xdp_prog_func(struct xdp_md *ctx)
 {
     struct datarec *rec; 
-    ___u32 key = XDP_PASS;
+    __u32 key = XDP_PASS;
     rec = bpf_map_lookup_elem(&xdp_stats_map , &key);
         if(!rec) // null pointer check 
             return XDP_ABORTED;
