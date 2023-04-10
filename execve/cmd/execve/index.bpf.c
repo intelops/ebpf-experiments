10afc4d399e8b21787cf4150671429ada93e8aab
AnamikaSanjay <asanjay@intelops.dev>
2023-04-10 17:16:26 -0400

    updating execve program to capture more data

--- execve/cmd/execve/index.bpf.c
+++ execve/cmd/execve/index.bpf.c
@@ -1,14 +1,31 @@
 //go:build ignore
 
+//Author: Charan Ravela
+//Start Date: 03-20-2023
+//Last Updated: 04-06-2023
+
 #include "vmlinux.h"
 #include "bpf_helpers.h"
 
-struct bpf_map_def SEC("maps") event = {
-    .type = BPF_MAP_TYPE_PERF_EVENT_ARRAY,
-    .key_size = sizeof(__u32),
-    .value_size = sizeof(__u32),
-    .max_entries = 1024,
+//
+struct event_data
+{
+    __u32 pid;
+    __u32 tgid;
+    __u32 uid;
+    __u32 gid;
+    __s32 syscall_nr;
+    __u8 comm[16];
+    __u8 cwd[32];
+    __u8 binary_filepath[256];
+    __u8 user_comm[256][256];
 };
+const struct event_data *unused __attribute__((unused));
+
+struct{
+    __uint(type, BPF_MAP_TYPE_RINGBUF);
+    __uint(max_entries, 1 << 24);
+} event SEC(".maps");
 
 //sys_enter_execve data structure
 //can be found at below path
@@ -26,40 +43,59 @@ struct execve_struct
     __u8 *const envp;
 };
 
-//
-struct event_data
-{
-    __u32 pid;
-    __u32 tgid;
-    __u32 uid;
-    __u32 gid;
-    __s32 syscall_nr;
-    __u8 comm[80];
-    __u8 filename[256];
-};
-
 SEC("tracepoint/syscalls/sys_enter_execve")
 int ebpf_execve(struct execve_struct *ctx){
-    struct event_data ed = {};
+    struct event_data *ed;
+    ed = bpf_ringbuf_reserve(&event, sizeof(struct event_data), 0);
+    if (!ed) {
+        return 0;
+    }
+    ed->syscall_nr = ctx->syscall_nr;
 
-    ed.syscall_nr = ctx->syscall_nr;
+    //fetches user command binary filepath    
+    bpf_probe_read_user(&ed->binary_filepath, sizeof(ed->binary_filepath), ctx->filename);
 
-    //reads filename    
-    bpf_probe_read_user(&ed.filename, sizeof(&ed.comm), ctx->filename);
-    
-    //fetch current command
-    bpf_get_current_comm(&ed.comm, sizeof(&ed.comm));
+    // fetch current command
+    bpf_get_current_comm(&ed->comm, sizeof(ed->comm));
 
+    //fetch process id and thread group id
     __u64 pid_tgid = bpf_get_current_pid_tgid();  
-    ed.pid = pid_tgid >> 32;
-    ed.tgid = pid_tgid;
+    ed->pid = pid_tgid >> 32;
+    ed->tgid = pid_tgid;
 
+    //fetch user id and group id
     __u64 uid_gid = bpf_get_current_uid_gid();
-    ed.uid = uid_gid >> 32;
-    ed.gid = uid_gid;
+    ed->uid = uid_gid >> 32;
+    ed->gid = uid_gid;
 
-    //pushes the information to perf event map
-    bpf_perf_event_output(ctx, &event, BPF_F_CURRENT_CPU, &ed, sizeof(ed));
+    //fetches current working directory
+    struct task_struct *task = (struct task_struct *)bpf_get_current_task();
+    struct fs_struct *fs;
+    struct dentry *dentry;
+
+    bpf_probe_read_kernel(&fs, sizeof(fs), &task->fs);
+    bpf_probe_read(&dentry, sizeof(dentry), &fs->pwd.dentry);
+    bpf_probe_read(&ed->cwd, sizeof(ed->cwd), &dentry->d_iname);
+
+    //fetches the user command
+    __u8 *filn;
+    int rs;
+    int i = 0, j = 0;
+
+    while (i <= 256){
+        bpf_probe_read(&filn, sizeof(filn), &ctx->argv[i]);
+        rs = bpf_probe_read(&ed->user_comm[j], sizeof(ed->user_comm[j]), filn);
+        
+        if (rs != 0){
+            break;
+        }
+
+        i = i + 8;
+        j++;
+    };
+
+    //pushes the information to ringbuf event map
+    bpf_ringbuf_submit(ed, 0);
 
     return 0;
 };
--- execve/cmd/execve/main.go
+++ execve/cmd/execve/main.go
@@ -1,59 +1,54 @@
+//Author: Charan Ravela
+//Start Date: 03-20-2023
+//Last Updated: 04-06-2023
+
 package main
 
 import (
 	"C"
 	"fmt"
-
-	"github.com/cilium/ebpf/link"
-	"github.com/cilium/ebpf/perf"
-	"github.com/cilium/ebpf/rlimit"
 )
 import (
 	"bytes"
 	"encoding/binary"
+	"os"
+
+	"github.com/cilium/ebpf/link"
+	"github.com/cilium/ebpf/ringbuf"
+	"github.com/cilium/ebpf/rlimit"
 )
 
-//Ebpf map item structure
-type event_data struct {
-	Pid        uint32
-	Tgid       uint32
-	Uid        uint32
-	Gid        uint32
-	Syscall_nr int32
-	Comm       [80]uint8
-	Filename   [256]uint8
-}
-
-//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -cc clang -cflags $BPF_CFLAGS bpf index.bpf.c -- -I../../headers
+//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -cc clang -cflags $BPF_CFLAGS -type event_data bpf index.bpf.c -- -I../../headers
 
 func main() {
 	//Removes memory lock limit of the ebpf program
 	err := rlimit.RemoveMemlock()
-	must("Error while removing the memlock", err)
+	must("Error while removing the memlock", err, true)
 
 	//Loads ebpf objects(programs, maps)
 	ebpfColl := bpfObjects{}
 	err = loadBpfObjects(&ebpfColl, nil)
-	must("Error while loading the ebpf object", err)
+	must("Error while loading the ebpf object", err, true)
+	defer ebpfColl.Close()
 
 	//Attach program to a hook
 	hook, err := link.Tracepoint("syscalls", "sys_enter_execve", ebpfColl.EbpfExecve, nil)
-	must("Error while attaching a ebpf program", err)
+	must("Error while attaching a ebpf program", err, true)
 	defer hook.Close()
 
-	//Create perf map reader
-	perfReader, err := perf.NewReader(ebpfColl.Event, 4096)
-	must("Error while creating map reader", err)
-	defer perfReader.Close()
+	//Create ringbuffer map reader
+	ringbufReader, err := ringbuf.NewReader(ebpfColl.Event)
+	must("Error while creating map reader", err, true)
+	defer ringbufReader.Close()
 
 	//reads data from map
-	mapDataEmitter := make(chan perf.Record)
+	mapDataEmitter := make(chan ringbuf.Record)
 	go func() {
 		defer close(mapDataEmitter)
 
 		for {
-			record, err := perfReader.Read()
-			must("Error while reading map", err)
+			record, err := ringbufReader.Read()
+			must("Error while reading map", err, true)
 
 			mapDataEmitter <- record
 		}
@@ -65,33 +60,61 @@ func main() {
 	for {
 		record := <-mapDataEmitter
 
-		var row event_data
+		var row bpfEventData
 		err = binary.Read(bytes.NewBuffer(record.RawSample), binary.LittleEndian, &row)
-		must("Error while parsing the data", err)
+		must("Error while parsing the data", err, true)
 
 		printToScreen(row)
 		prompt("Waiting for event to trigger!")
 	}
 }
 
-func must(msg string, err error) {
+func must(msg string, err error, shouldTerminte bool) {
 	if err != nil {
-		fmt.Printf("%s : %v", msg, err)
+		fmt.Printf("\n%s : %v\n", msg, err)
+		if shouldTerminte {
+			os.Exit(1)
+		}
 	}
 }
 
-func printToScreen(row event_data) {
+func printToScreen(row bpfEventData) {
 	fmt.Println("-----------------------------------------")
-	fmt.Printf("Process Id: %d\n", row.Pid)
-	fmt.Printf("Thread Id: %d\n", row.Tgid)
 	fmt.Printf("User Id: %d\n", row.Uid)
 	fmt.Printf("Group Id: %d\n", row.Gid)
-	fmt.Printf("Syscall Number: %d\n", row.Syscall_nr)
 	fmt.Printf("Command: %s\n", row.Comm)
-	fmt.Printf("Filename: %s\n", row.Filename)
+	fmt.Printf("Syscall Number: %d\n", row.SyscallNr)
+	fmt.Printf("Process Id: %d\n", row.Pid)
+	fmt.Printf("Thread Group Id: %d\n", row.Tgid)
+	fmt.Printf("Current Working Directory: %s\n", sanitizeText(string(row.Cwd[:])))
+	fmt.Printf("Binary Filepath: %s\n", sanitizeText(string(row.BinaryFilepath[:])))
+	fmt.Printf("User Command: %v\n", sanitizeUserCommand(row.UserComm))
 	fmt.Println("-----------------------------------------")
 }
 
 func prompt(msg string) {
-	fmt.Printf("\n %s \r", msg)
+	fmt.Printf("\n%s \r", msg)
+}
+
+//removes the null characters from the end of string
+func sanitizeText(str string) (restr string) {
+	strb := []byte(str)
+	restr = string(bytes.Split(strb[:], []byte("\x00"))[0])
+
+	return
+}
+
+//removes the null characters from array elements
+//and removes the empty array elements
+func sanitizeUserCommand(tdarr [256][256]uint8) []string {
+	var comms []string
+
+	for _, ele := range tdarr {
+		str := sanitizeText(string(ele[:]))
+		if len(str) != 0 {
+			comms = append(comms, str)
+		}
+	}
+
+	return comms
 }
